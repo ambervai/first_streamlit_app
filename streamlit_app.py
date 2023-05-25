8a7a728cb13002126e1469fda161ab311009a249
ambervai <131892882+ambervai@users.noreply.github.com>
2023-05-25 14:36:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,9 +61,6 @@ except URLError as e:
 
 #streamlit.text(fruityvice_response.json()) # just writes the data to the screen -- needs removed from the screen as it is raw json data
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -78,10 +75,13 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding" + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to  add?')
-if streamlit.button('Add a Frui to the List'):
+if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
+  
+# don't run anything past here while we troubleshoot
+streamlit.stop()
   
 #streamlit.write
 
